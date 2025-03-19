14a10db767e961dc20c20f9ee0c89fb12c754d7f
Pablo Moreno <pablonicolasmc5@gmail.com>
2025-03-19 18:41:28 -0400

    feat: updated React, added componentsn and applied styles

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,12 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import App from "../components/App";
+import { createRoot } from 'react-dom/client';
+import "../public/styles.css";  
+
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
