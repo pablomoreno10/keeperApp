8a3a67a63810fa0175e8cc21698721fd9fd86117
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-04-02 14:52:36 -0400

    refactor: remove notes.jsx and update imports

--- components/App.jsx
+++ components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../src/notes";
+import notes from "./notes";
 
 function App(){
     return (
