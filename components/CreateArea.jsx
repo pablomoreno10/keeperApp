c4de394007da9d3bc67bc747f93399a79d61a10e
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-04-03 16:39:29 -0400

    feat: add button functionality, create new notes

--- components/CreateArea.jsx
+++ components/CreateArea.jsx
@@ -1,33 +1,38 @@
 import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
 
   const [note, setNote] = useState({
     title: "",
     content: ""
-  })
+  });
 
   function handleChange(event){
     const {name, value} = event.target;
 
     setNote(prevNote => {
       return {
-        ...prevNote, 
-        [name]:value
+        ...prevNote,
+        [name]: value
       };
-      });
+    });
   }
 
-  function addNote(event){
+  function submitNote(event){
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
     event.preventDefault();
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitNote}>
         <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
         <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={addNote}>add</button>
+        <button>add</button>
       </form>
     </div>
   );
